aafab093140931f02e76d4026d15d145598a0469
kix氏 <kix@NW005PC.local>
2014-11-09 22:28:30 +0900

    ログ出力

--- imageGet.php
+++ imageGet.php
@@ -33,6 +33,7 @@ $maxResults = 100;
 $userId = $_POST["userID"];
 $url = "https://www.googleapis.com/plus/v1/people/" . $userId . "/activities/public?maxResults=100&key=AIzaSyD0v2NJR22_He4zS9BzwnJQVSpSQNHSn3g";
 $emichi = json_decode(file_get_contents($url));
+print_r($emichi);
 $nextPage = $emichi -> {'nextPageToken'};
 foreach($emichi -> {'items'} as $data){
   $name = mb_convert_encoding($data -> {'actor'} -> {'displayName'}, "UTF-8", "auto");
@@ -62,14 +63,14 @@ while($nextPage != ""){
   }
   if($_POST["type"] == 3){ //もっと古い画像を保存
     if($pageCount > 26){
-      gplusSave($emichi,0,$imageDirectory);
+      echo gplusSave($emichi,0,$imageDirectory);
     }
   }elseif($_POST["type"] == 2){ //古い画像を保存
     if($pageCount > 13){
-      gplusSave($emichi,0,$imageDirectory);
+      echo gplusSave($emichi,0,$imageDirectory);
     }
   }else{
-    gplusSave($emichi,0,$imageDirectory);
+    echo gplusSave($emichi,0,$imageDirectory);
   }
 }
 
@@ -91,6 +92,8 @@ function gplusSave($apiData,$typeFlag,$imageDirectory){
               if($typeFlag == 1){
                 $url = mb_convert_encoding($data2 -> {'image'} -> {'url'}, "UTF-8" , "auto");
                 $returnText .= "<a href='" . $fullUrl . "'><img src='" . $url . "'></a>";
+              }else{
+                $returnText .= $dlUrl . "の画像を保存しました<br>";
               }
 
               $imgData = curl_init();
@@ -99,7 +102,6 @@ function gplusSave($apiData,$typeFlag,$imageDirectory){
               $data = curl_exec($imgData);
               file_put_contents($dlUrl, $data);
               curl_close($imgData);
-              echo "ファイルサイズ" . filesize($dlUrl) . "<br>";
             }
           }
         }
