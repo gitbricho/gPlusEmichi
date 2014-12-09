cf5a526da9033326de4f92aa0453632d3fb0e532
kix氏 <kix@NW005PC.local>
2014-12-10 00:06:28 +0900

    DIRECTORY_SEPARATOR

--- imageGet.php
+++ imageGet.php
@@ -105,7 +105,7 @@ function gplusSave($apiData,$typeFlag,$imageDirectory){
       foreach($data -> {'object'} -> {'attachments'} as $data2){
         if(!empty($data2 -> {'image'} -> {'url'})){
           if($data2 -> {'image'} -> {'url'} != ""){
-            $dlUrl = "images" . DIRECTORY_SEPARATO . $imageDirectory . DIRECTORY_SEPARATO . $datetime . ".jpg";
+            $dlUrl = "images" . DIRECTORY_SEPARATOR . $imageDirectory . DIRECTORY_SEPARATOR . $datetime . ".jpg";
             if(!file_exists($dlUrl)){
               if(!empty($data2 -> {'fullImage'} -> {'url'})){
                 $fullUrl = mb_convert_encoding($data2 -> {'fullImage'} -> {'url'}, $osFlag , "ASCII,JIS,UTF-8,EUC-JP,SJIS");
